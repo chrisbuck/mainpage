2149e9a1f38b5eb2a87df93a946fef1f9fa30dc3
chrisbuck <chrisbuck29@gmail.com>
2017-05-24 13:19:06 -0400

    autoplay

--- demos/ksvid.js
+++ demos/ksvid.js
@@ -13,7 +13,7 @@ function sizeVid(myW, myH){
     var myhtml = '<div id="vidDiv" style="background-color: #000000; border: 1px solid #383838; width: ' + thisW + 'px;">';
     myhtml += '<div id="vidInfo" style="border: 1px solid #ff0000;"><span style="color: #ff0000; font-weight: bold;">Notice: </span>';
     myhtml += '<span style="color: #ffffff;">This video is confidential. Please do not distribute to anyone else.</span></div>';
-    myhtml += '<video width="' + thisW + 'px" height="' + thisH + 'px" controls="true">';
+    myhtml += '<video width="' + thisW + 'px" height="' + thisH + 'px" controls="true" autoplay>';
     myhtml += '<source src="https://www.dropbox.com/s/rvup0myx0pxd6wy/gwizzy-kickstarter-HD-43.mp4?dl=1" type="video/mp4">';
     myhtml += '</video></div>';
  
